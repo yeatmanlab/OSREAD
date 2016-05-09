00b434dc7a2958dee57b940a07b46de16ce6034a
Kha Nguyen <ykha@uw.edu>
2016-05-09 15:45:26 -0700

    first commit, add SecondExample

--- grails-app/services/appforliteracy/ModuleListService.groovy
+++ grails-app/services/appforliteracy/ModuleListService.groovy
@@ -5,7 +5,7 @@ class ModuleListService {
     static List<String> getModuleNames() {
         List<String> names = new ArrayList<>()
         names.add("FirstExample")
-
+	names.add("SecondExample")
         //TODO: Add additional module names here
 
         return names
