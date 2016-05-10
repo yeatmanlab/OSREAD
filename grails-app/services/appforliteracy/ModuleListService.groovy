099c8252b12e23749e065e9758142600bca98bbf
Kha Nguyen <ykha@uw.edu>
2016-05-09 17:25:02 -0700

    change everything back

--- grails-app/services/appforliteracy/ModuleListService.groovy
+++ grails-app/services/appforliteracy/ModuleListService.groovy
@@ -5,7 +5,6 @@ class ModuleListService {
     static List<String> getModuleNames() {
         List<String> names = new ArrayList<>()
         names.add("FirstExample")
-	names.add("SecondExample")
         //TODO: Add additional module names here
 
         return names
