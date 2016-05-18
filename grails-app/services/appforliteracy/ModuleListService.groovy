bc2837ce225e96d9685096e3737ccbd5569ffeb6
surgepower <power.surge43@gmail.com>
2016-05-17 20:21:53 -0700

    Added types
    Added the modules that we currently have

--- grails-app/services/appforliteracy/ModuleListService.groovy
+++ grails-app/services/appforliteracy/ModuleListService.groovy
@@ -7,6 +7,8 @@ class ModuleListService {
         names.add("FirstExample")
         names.add("SecondExample")
         names.add("Bexample")
+        names.add("FifthExample")
+        names.add("ThirdExample")
 
         //TODO: Add additional module names here
 
